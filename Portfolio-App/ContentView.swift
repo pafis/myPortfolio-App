f7bfea28c762bc9fdf0e87ecec6bfe577542a300
Pascal Fischer <pascalfischer@me.com>
2023-06-16 11:35:31 +0200

    Initial Commit

---
+++ Portfolio-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Portfolio-App
+//
+//  Created by Pascal Fischer on 16.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Portfolio-App/Portfolio_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_AppApp.swift
+//  Portfolio-App
+//
+//  Created by Pascal Fischer on 16.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
